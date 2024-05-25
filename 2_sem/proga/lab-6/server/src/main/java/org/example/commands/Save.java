283a96cfd2bf040fac46df4bd4db59f337940969
lil-kitten <r.ievlev123@mail.ru>
2024-05-25 13:16:30 +0300

    супер комит

--- 2_sem/proga/lab-6/server/src/main/java/org/example/commands/Save.java
+++ 2_sem/proga/lab-6/server/src/main/java/org/example/commands/Save.java
@@ -12,13 +12,11 @@ import org.example.managers.FileManager;
  * save collection in file
  */
 public class Save extends Command {
-    private Printable console;
     private FileManager fileManager;
     private CollectionManager collectionManager;
 
-    public Save(Printable console, FileManager fileManager, CollectionManager collectionManager) {
+    public Save( FileManager fileManager, CollectionManager collectionManager) {
         super("save", ": сохранить коллекцию в файл");
-        this.console = console;
         this.fileManager = fileManager;
         this.collectionManager = collectionManager;
     }
@@ -27,7 +25,6 @@ public class Save extends Command {
     public Response execute(Request request) {
         fileManager.save();
         collectionManager.setLastSaveDate();
-        console.println(ConsoleColor.GREEN + "супер, все сохранилось в текущий файлик =)");
         return new Response(ResponseStatus.OK,ConsoleColor.GREEN + "супер, все сохранилось в текущий файлик =)");
     }
 }
